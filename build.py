55fec99bb91b15aa61cc094c7a7449e6675cf9e4
adecesa1 <adecesa1@cs3280-vm06.uwg.westga.edu>
2020-11-22 15:50:44 -0500

    pushed the inital files

---
+++ build.py
@@ -0,0 +1,35 @@
+#   -*-coding: utf-8 -*-
+from pybuilder.core import use_plugin, init, task, depends
+from os import path
+from subprocess import call
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+#use_plugin("python.flake8")
+#use_plugin("python.coverage")
+use_plugin("python.distutils")
+use_plugin("copy_resources")
+
+name ="lecture25"
+default_task ="publish"
+# target directory (relative)
+target_dir =path.join('target', 'dist', 'app')
+#@init
+#def set_properties(project):
+#    pass
+
+@init
+def normalize_target_dir(project):
+    project.set_property('dir_dist', target_dir)
+    
+@init
+def copy_html_pages_to_distribution(project):
+    project.set_property('copy_resources_target', target_dir)
+    project.set_property('copy_resources_glob', ['*.html', '*.HTML', '*.htm', '*.HTM'])
+
+@depends('publish')
+@task
+def launch_server(project):
+    print("Note: This webserver is not running in a container.")
+    print("Note: A webpage should be available at http://127.0.0.1:3280/index.html")
+    call(['python',path.join(target_dir, 'server.py')])
